eb922c90803b7a1b0bcfa0d5ff7a8834d1de8842
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2026-02-10 21:51:27 +0000

    Update src/inference/differences.py

--- src/inference/differences.py
+++ src/inference/differences.py
@@ -1,10 +1,12 @@
+"""Module differences.py"""
 import logging
 import os
+
 import pandas as pd
 
 import config
-import src.functions.objects
 import src.elements.specification as sc
+import src.functions.objects
 
 
 class Differences:
@@ -20,7 +22,7 @@ class Differences:
         self.__objects = src.functions.objects.Objects()
         self.__configurations = config.Config()
 
-    def __get_quantiles(self, uri: str):
+    def __get_quantiles(self, uri: str) -> dict:
         """
 
         :param uri:
@@ -29,7 +31,7 @@ class Differences:
 
         data = self.__objects.read(uri=uri)['q_testing']
 
-        return {k: v for k, v in zip(data['columns'], data['data'])}
+        return  dict(zip(data['columns'], data['data']))
 
     def exc(self, estimates: pd.DataFrame, specification: sc.Specification):
         """
@@ -45,4 +47,5 @@ class Differences:
         quantiles = self.__get_quantiles(uri=uri)
         logging.info(quantiles)
 
+        estimates.info()
         logging.info(estimates.head())
