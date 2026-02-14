0bef5a98c0b7d58f56609d61892c05018b17d7d9
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2026-02-14 20:31:08 +0000

    Update src/inference/interface.py

--- src/inference/interface.py
+++ src/inference/interface.py
@@ -7,7 +7,7 @@ import src.elements.attribute as atr
 import src.elements.master as mr
 import src.elements.specification as sc
 import src.inference.approximating
-import src.inference.differences
+import src.inference.plausible_anomalies
 import src.inference.scaling
 
 
@@ -27,7 +27,7 @@ class Interface:
         # Setting up
         self.__scaling = src.inference.scaling.Scaling()
         self.__approximating = src.inference.approximating.Approximating()
-        self.__differences = src.inference.differences.Differences()
+        self.__plausible_anomalies = src.inference.plausible_anomalies.PlausibleAnomalies()
 
     def exc(self, attribute: atr.Attribute, data: pd.DataFrame, specification: sc.Specification) -> pd.DataFrame:
         """
@@ -42,7 +42,7 @@ class Interface:
         master: mr.Master = mr.Master(data=data, transforms=transforms)
         estimates: pd.DataFrame = self.__approximating.exc(
             specification=specification, attribute=attribute, master=master)
-        estimates: pd.DataFrame = self.__differences.exc(
+        estimates: pd.DataFrame = self.__plausible_anomalies.exc(
             estimates=estimates.copy(), specification=specification)
 
         return estimates
