0820d9f86741cbc7f46be5b509bc80ee3447ae6a
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2026-02-14 19:58:59 +0000

    Update src/algorithms/interface.py

--- src/algorithms/interface.py
+++ src/algorithms/interface.py
@@ -7,6 +7,7 @@ import pandas as pd
 
 import src.algorithms.attributes
 import src.algorithms.data
+import src.algorithms.gap
 import src.algorithms.persist
 import src.elements.attribute as atr
 import src.elements.specification as sc
@@ -39,6 +40,7 @@ class Interface:
         __get_attributes = dask.delayed(src.algorithms.attributes.Attributes().exc)
         __get_data = dask.delayed(src.algorithms.data.Data(arguments=self.__arguments).exc)
         __get_special_anomalies = dask.delayed(src.inference.interface.Interface(arguments=self.__arguments).exc)
+        __gap = dask.delayed(src.algorithms.gap.Gap(arguments=self.__arguments).exc)
         __persist = dask.delayed(src.algorithms.persist.Persist().exc)
 
         computations = []
@@ -46,6 +48,7 @@ class Interface:
             attribute: atr.Attribute = __get_attributes(specification=specification)
             data: pd.DataFrame = __get_data(specification=specification, attribute=attribute)
             estimates: pd.DataFrame = __get_special_anomalies(attribute=attribute, data=data, specification=specification)
+            estimates: pd.DataFrame = __gap(estimates=estimates)
 
             message = __persist(specification=specification, estimates=estimates)
             computations.append(message)
