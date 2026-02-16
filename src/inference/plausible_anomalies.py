147d068ae06dfd2aba7c08d62c202df7d66c977f
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2026-02-16 13:03:06 +0000

    Update src/inference/plausible_anomalies.py

--- src/inference/plausible_anomalies.py
+++ src/inference/plausible_anomalies.py
@@ -16,29 +16,41 @@ class PlausibleAnomalies:
 
     def __init__(self):
         """
+
         Constructor
         """
 
         self.__objects = src.functions.objects.Objects()
         self.__configurations = config.Config()
 
-    def __plausible_anomalies(self, estimates: pd.DataFrame, specification: sc.Specification) -> np.ndarray:
+    def __get_error_quantiles(self, ts_id: int):
         """
         {k: v for k, v in zip(data['columns'], data['data'][0])}
 
+        :param ts_id:
+        :return:
+        """
+
+        # Error quantiles
+        uri = os.path.join(self.__configurations.data_, 'metrics', str(ts_id) + '.json')
+        data = self.__objects.read(uri=uri)['q_testing']
+        quantiles = dict(zip(data['columns'], data['data'][0]))
+
+        return quantiles
+
+    def __plausible_anomalies(self, estimates: pd.DataFrame, ts_id: int) -> np.ndarray:
+        """
+
         :param estimates:
-        :param specification:
+        :param ts_id:
         :return:
         """
 
         points: np.ndarray = estimates['p_error'].values
         real: np.ndarray = estimates['original'].notna().values
 
-        # Error quantiles
-        uri = os.path.join(self.__configurations.data_, 'metrics', str(specification.ts_id) + '.json')
-        data = self.__objects.read(uri=uri)['q_testing']
-        quantiles = dict(zip(data['columns'], data['data'][0]))
-
+        # Quantiles & Boundaries
+        quantiles = self.__get_error_quantiles(ts_id=ts_id)
         median = quantiles.get('median')
         l_boundary = quantiles.get('l_whisker_e') - (median - quantiles.get('l_whisker_e'))
         u_boundary = quantiles.get('u_whisker_e') + (quantiles.get('u_whisker_e') - median)
@@ -57,8 +69,7 @@ class PlausibleAnomalies:
         :return:
         """
 
-
-        p_anomalies = self.__plausible_anomalies(estimates=estimates.copy(), specification=specification)
+        p_anomalies = self.__plausible_anomalies(estimates=estimates.copy(), ts_id=specification.ts_id)
         estimates = estimates.assign(p_anomaly=p_anomalies)
 
         return estimates
