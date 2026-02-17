9c5c1e90d1f823f8a041c962a60bf9d7500a57da
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2026-02-17 10:20:20 +0000

    Update src/inference/interface.py

--- src/inference/interface.py
+++ src/inference/interface.py
@@ -1,9 +1,11 @@
 """Module inference/interface.py"""
 
+import boto3
 import pandas as pd
 
 import src.elements.attribute as atr
 import src.elements.master as mr
+import src.elements.s3_parameters as s3p
 import src.elements.specification as sc
 import src.inference.approximating
 import src.inference.plausible_anomalies
@@ -15,16 +17,20 @@ class Interface:
     Interface
     """
 
-    def __init__(self):
+    def __init__(self, connector: boto3.session.Session, s3_parameters: s3p.S3Parameters, arguments: dict):
         """
 
-        Constructor
+        :param connector: An instance of boto3.session.Session<br>
+        :param s3_parameters: The overarching S3 parameters settings of this
+                              project, e.g., region code name, buckets, etc.<br>
+        :param arguments: A set of arguments vis-à-vis computation & storage objectives.<br>
         """
 
         # Setting up
         self.__scaling = src.inference.scaling.Scaling()
         self.__approximating = src.inference.approximating.Approximating()
-        self.__plausible_anomalies = src.inference.plausible_anomalies.PlausibleAnomalies()
+        self.__plausible_anomalies = src.inference.plausible_anomalies.PlausibleAnomalies(
+            connector=connector, s3_parameters=s3_parameters, arguments=arguments)
 
     def exc(self, attribute: atr.Attribute, data: pd.DataFrame, specification: sc.Specification) -> pd.DataFrame:
         """
