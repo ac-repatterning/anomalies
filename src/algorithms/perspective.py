c255141d5ce5ad3e9a68d8f716a4c56c0da0381d
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2026-02-18 13:43:47 +0000

    Update src/algorithms/perspective.py: In Progress

--- src/algorithms/perspective.py
+++ src/algorithms/perspective.py
@@ -1,21 +1,37 @@
-
+"""Module perspective.py"""
 import pandas as pd
 
 import src.elements.specification as sc
 
 
 class Perspective:
+    """
+
+    An overarching summary of the number of time points that each series' gaps, missing
+    points, asymptotes, etc, spans.
+    """
 
     def __init__(self):
+        """
+
+        Constructor
+        """
 
         self.__names = ['p_anomaly', 'gap', 'missing', 'asymptote', 'extreme']
 
-    def exc(self, frame: pd.DataFrame, specification: sc.Specification):
+    def exc(self, frame: pd.DataFrame, specification: sc.Specification) -> dict:
+        """
+
+        :param frame:
+        :param specification: Refer to src/elements/specification.py
+        :return:
+        """
 
         data: pd.DataFrame = frame.copy()[self.__names]
         matrix: pd.DataFrame = data.gt(0)
-
         vector = dict(matrix.sum(axis=0))
-        print(vector.update(specification._asdict()))
+
+        vector.update(specification._asdict())
+        vector['p_starting'] = frame['timestamp'].min()
 
         return vector
