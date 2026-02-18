9fb345165b4e79d8d765304e7e4b40fe53b25174
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2026-02-18 13:54:12 +0000

    Update src/algorithms/perspective.py: In Progress

--- src/algorithms/perspective.py
+++ src/algorithms/perspective.py
@@ -1,6 +1,33 @@
+import os
+
+import pandas as pd
+
+import config
 
 
 class Perspective:
 
     def __init__(self):
-        pass
+        """
+
+        Constructor
+        """
+
+        self.__configurations = config.Config()
+
+    def exc(self, vectors: list[dict]):
+        """
+
+        :param vectors:
+        :return:
+        """
+
+        records = pd.DataFrame.from_records(vectors)
+        records.drop(columns=['ts_name', 'starting'], inplace=True)
+
+        try:
+            records.to_json(
+                path_or_buf=os.path.join(self.__configurations.perspective_, 'perspective.json'),
+                orient='index')
+        except OSError as err:
+            raise err from err
