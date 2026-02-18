a6282adc9e1a622724dd4afa276c736132677a26
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2026-02-18 11:09:08 +0000

    Update src/algorithms/perspective.py: In Progress

--- src/algorithms/perspective.py
+++ src/algorithms/perspective.py
@@ -8,9 +8,14 @@ class Perspective:
 
     def __init__(self):
 
-        self.__names = ['p_anomaly', 'gap', 'asymptote', 'extreme']
+        self.__names = ['p_anomaly', 'gap', 'missing', 'asymptote', 'extreme']
 
     def exc(self, frame: pd.DataFrame, specification: sc.Specification):
 
-        data = frame.copy()[self.__names]
-        data.info()
+        data: pd.DataFrame = frame.copy()[self.__names]
+        matrix: pd.DataFrame = data.gt(0)
+
+        vector = dict(matrix.sum(axis=0))
+        print(vector.update(specification._asdict()))
+
+        return vector
