e1878ae15a7e6429f916fe8ab3c0755fb32eb129
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2026-02-18 14:34:19 +0000

    Update src/algorithms/perspective.py

--- src/algorithms/perspective.py
+++ src/algorithms/perspective.py
@@ -29,11 +29,11 @@ class Perspective:
         """
 
         records = pd.DataFrame.from_records(vectors)
-        records.drop(columns=['ts_name', 'starting'], inplace=True)
+        records.drop(columns=['ts_name'], inplace=True)
 
         try:
             records.to_json(
                 path_or_buf=os.path.join(self.__configurations.perspective_, 'perspective.json'),
-                orient='index')
+                orient='split')
         except OSError as err:
             raise err from err
